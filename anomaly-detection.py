add scaling, fix base/fed comparison

anomaly-detection.py
@@ -15,6 +15,7 @@ m = 0.9
 device = "cuda"
 criterion = nn.MSELoss()
 
+
 baseline_sampler = DataSampler(num_tot_samples, [["ras4-8gb", ["normal"]], ["ras3", ["normal", "delay", "disorder"]]])
 aggregator = AutoencoderUpdate(baseline_sampler, batch_size, epochs, learning_rate, criterion)
 net = AutoEncoder(K=75).to(device)
@@ -28,21 +29,8 @@ if train:
     # training
     mdict, loss = aggregator.update_weights(net)
     # inference
-    acc, test_loss = aggregator.inference(net)
+    corrects, totals, test_loss = aggregator.inference(net)
     torch.save(net.state_dict(), "global-mlp.model")
-    print(f"Final accuracy after {epochs} epochs: {acc}")
+    print(f"Final accuracy after {epochs} epochs: {corrects / totals}")
 else:
     net.load_state_dict(torch.load("global-mlp.model"))
-    #tacc = test_loop(test_loader, net, loss_fn)
-    #print(f"Done! highest accuracy ever achieved is: {tacc}")
-
-
-
-# train = True
-#
-# if train:
-#     acc = 0
-#     print(f"Final accuracy after {epochs} epochs: {acc}")
-#
-# else:
-#     net.load_state_dict(torch.load("global-autoencoder.model"))

binary-mlp.py
@@ -2,22 +2,35 @@ import torch
 from torch import nn
 from models import MLP
 from datasampling import DataSampler
-from localops import BinaryUpdate
+from localops import BinaryOps
+from utils import get_sampler_data, get_baseline_data, scale_baseline, scale_federation
+from federated import train_model
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+import copy
 
 ## Test Experiment for centralized binary classification
 
 # select some hyperparameters and initialize network
 learning_rate = 1e-5
 batch_size = 64
-epochs = 20
+epochs = 30
 num_tot_samples = 6000
 device = "cuda"
+split = 0.8
 
 net = MLP(K=75).to(device)
-#print(net)
+print(net)
 
-baseline_sampler = DataSampler(num_tot_samples, [("ras4-4gb", ["normal"]), ("ras3", ["normal", "delay", "disorder"])])
-aggregator = BinaryUpdate(baseline_sampler, batch_size, epochs, learning_rate)
+baseline_sampler = DataSampler(num_tot_samples, [("ras4-4gb", ["normal"]),
+                                                 ("ras3", ["normal", "delay", "disorder"])])
+# get the balanced data and targets per participant, split into train and testing parts
+baseline_data = get_baseline_data(get_sampler_data([baseline_sampler], split=split))
+
+# standardize data
+scaler = MinMaxScaler() # TRY OUT with StandardScaler()
+baseline_data, scaler = scale_baseline(baseline_data, scaler)
+aggregator = BinaryOps(baseline_data, batch_size, epochs, learning_rate)
 
 train = True
 if train:
@@ -25,13 +38,9 @@ if train:
     # training
     mdict, loss = aggregator.update_weights(net)
     # inference
-    acc, test_loss = aggregator.inference(net)
+    corrects, totals, test_loss = aggregator.inference(net)
     torch.save(net.state_dict(), "global-mlp.model")
-    print(f"Final accuracy after {epochs} epochs: {acc}")
+    print(f"\n\nFinal accuracy after {epochs} epochs: {100*corrects / totals}%")
 else:
     net.load_state_dict(torch.load("global-mlp.model"))
-    #tacc = test_loop(test_loader, net, loss_fn)
-    #print(f"Done! highest accuracy ever achieved is: {tacc}")
-
-
 

federated.py
@@ -2,7 +2,7 @@ import copy
 import torch
 from torch import nn
 import numpy as np
-from utils import average_weights, get_total_sample_accuracy, get_averaged_accuracy_for_participants
+from utils import average_weights
 
 
 def train_model(global_model, participants, epochs):
@@ -33,14 +33,16 @@ def train_model(global_model, participants, epochs):
         loss_avg = sum(local_losses) / len(local_losses)
         train_loss.append(loss_avg)
 
-        # Calculate avg training accuracy over all users at every epoch
+        # Calculate avg test accuracy over all users at every epoch
         list_acc, list_loss = [], []
         global_model.eval()
         for p in participants:
-            acc, loss = p.inference(model=global_model)
-            list_acc.append(acc)
+            corrects, totals, loss = p.inference(model=global_model)
+            list_acc.append(corrects / totals)
             list_loss.append(loss)
         train_accuracy.append(sum(list_acc) / len(list_acc))
+
+        print(f"loss {list_loss}")
 
         # print global training loss after every 'i' rounds
         if (epoch + 1) % print_every == 0:
@@ -51,54 +53,53 @@ def train_model(global_model, participants, epochs):
     return train_accuracy, train_loss
 
 def print_test_results(participants, global_model, epochs):
-    test_losses, test_corrects, test_totals = test_inference_xdata(participants, global_model)
-    avg_test_loss = sum(test_losses) / len(test_losses)
-    test_accuracy_avg = get_averaged_accuracy_for_participants(test_corrects, test_totals)
-    test_accuracy_tot = get_total_sample_accuracy(test_corrects, test_totals)
-    print(f'\nResults after {epochs} global rounds of training:')
-    print("|---- Avg Test Accuracy: {:.2f}%".format(100 * test_accuracy_avg))
-    print("|---- Tot Test Accuracy: {:.2f}%\n".format(100 * test_accuracy_tot))
+    if len(participants) == 1:
+        print(f'\nBaseline model results after {epochs} global rounds of training:')
+    else:
+        print(f'\nFederated model results after {epochs} global rounds of training:')
+    test_acc, test_loss = test_inference_xdata(participants, global_model)
+    print("|---- Avg Test Loss: {:.4f}".format(test_loss))
+    print("|---- Test Accuracy: {:.2f}%".format(100 * test_acc))
 
 
-def test_inference_xdata(participants, model):
-    """ Returns the test accuracy and loss for the global test split of all participants (list of LocalOps objects)
-    It is possible to create an arbitrary dataset (e.g. not the one used for training) for testing
-    In order to do this you must create samplers and localOps
-    like this for a global aggregator test:
-    baseline_sampler = DataSampler(6000, [["ras4-8gb", ["normal"]], ["ras3", ["normal", "delay", "disorder"]]])
-    aggregator = [BinaryUpdate(baseline_sampler, 64, 20, lr=learning_rate)]
-    call: test_inference_xdata(aggregator, model)
-
-    like this for a federated test:
-    # define participants and what data they contribute to the model
-    samplers = [DataSampler(loc_sample_size, [["ras4-8gb", ["normal"]]]),
-                DataSampler(loc_sample_size, [["ras3", ["normal", "delay", "disorder"]]])]
-    # initialize list of participants and their own unique test splits
-    participants = [BinaryUpdate(s, batch_size, loc_epochs=loc_epochs, lr=lr) for s in samplers]
-    call: test_inference_xdata(participants, model)
+def test_inference_xdata(participants, model, glob_avg=True):
     """
+    participants is a list of LocalOps objects, upon which inference() can be called,
+    model is a just a trained model
+
+    Returns the test accuracy and loss for the global test split of all participants (list of LocalOps objects)
+    It is possible to create an arbitrary dataset (e.g. not the one used for training) for testing.
+    This is just a helper function.
+    """
+
+    # TODO: adapt this function & inference() functions to get TPR/FPR etc
+    tot_acc, tot_loss = 0.0, 0.0
+    list_corrects, list_totals, list_loss = [], [], []
     model.eval()
-    losses, totals, corrects = [], [], []
-    device = 'cuda'
-
-    criterion = nn.BCEWithLogitsLoss().to(device)
     for p in participants:
-        plosses, ptotal, pcorrect = 0.0, 0.0, 0.0
-        for batch_idx, (x, y) in enumerate(p.glob_testloader):
-            x, y = x.to(device), y.to(device)
+        corrects, totals, loss = p.inference(model=model)
+        list_corrects.append(corrects)
+        list_totals.append(totals)
+        list_loss.append(loss)
 
-            # Inference
-            pred = model(x)
-            batch_loss = criterion(pred, y)
-            plosses += batch_loss.item()
+    if glob_avg == True:
+        tot_acc = get_total_sample_accuracy(list_corrects, list_totals)
+    else:
+        tot_acc = get_averaged_accuracy_for_participants(list_corrects, list_totals)
+    # loss already batch averaged by torch
+    tot_loss = sum(list_loss)
 
-            # Prediction Binary
-            pred[pred < 0.5] = 0
-            pred[pred > 0.5] = 1
-            pcorrect += (pred == y).type(torch.float).sum().item()
-            ptotal += len(y)
-        losses.append(plosses / len(p.glob_testloader))  # to ensure to return average losses per participant
-        totals.append(ptotal)
-        corrects.append(pcorrect)
+    return tot_acc, tot_loss
 
-    return losses, corrects, totals
+
+
+# choice 1 for federated accuracy calculation: distinction matters in case of unequal number of participant samples
+def get_total_sample_accuracy(corrects, totals):
+    return sum(corrects) / sum(totals)
+
+# choice 2 for federated accuracy calculation
+def get_averaged_accuracy_for_participants(corrects, totals):
+    acc = 0.0
+    for c, t in zip(corrects, totals):
+        acc += c/t
+    return acc / len(corrects)

localops.py
@@ -12,55 +12,34 @@ import numpy as np
 #  adaptions for autoencoder -> threshold selection, different standardization?
 
 class LocalOps(object):
-    def __init__(self, p : DataSampler, batch_size, epochs, lr):
+    def __init__(self, pdata, batch_size, epochs, lr):
         self.lr = lr
         self.epochs = epochs
         self.batch_size = batch_size
-        data, targets = p.sample()
-        self.trainloader, self.loc_testloader, self.glob_testloader = self.train_test_split(data, targets)
+        self.trainloader, self.testloader = self.train_test_split(pdata)
         self.device = "cuda"
         # Default criterion set to BCEWithlogits loss function (combines BCEloss and softmax layer, numerically stable)
 
-    def train_test_split(self, x, y):
+    def train_test_split(self, pdata):
         """
-        Returns train and test dataloaders for a given dataset, x & y
+        Returns train and test dataloaders for a given data and targets
         """
-        # split data
-        idxs = np.arange(len(y))
-        np.random.shuffle(idxs)
-        train_split = 0.8
-        loc_test_split = 0.1
-        idxs_train = idxs[:int(train_split*len(idxs))]
-        idxs_loc_test = idxs[int(train_split*len(idxs)):int((train_split+loc_test_split)*len(idxs))]
-        idxs_test = idxs[int((train_split+loc_test_split)*len(idxs)):]
-        x_train, y_train = x[idxs_train], y[idxs_train]
-        x_loc_test, y_loc_test = x[idxs_loc_test], y[idxs_loc_test]
-        x_test, y_test = x[idxs_test], y[idxs_test]
+        x_train, y_train, x_test, y_test = pdata
+        x_train, y_train = torch.from_numpy(x_train).float(), torch.from_numpy(y_train).float()
+        x_test, y_test = torch.from_numpy(x_test).float(), torch.from_numpy(y_test).float()
 
-        # TODO: share scale params in federation
-        # scale with training split mean/std
-        scaler = StandardScaler()
-        scaler.fit(x_train)
-        x_train = torch.from_numpy(scaler.transform(x_train)).float()
-        x_loc_test = torch.from_numpy(scaler.transform(x_loc_test)).float()
-        x_test = torch.from_numpy(scaler.transform(x_test)).float()
-        y_train = torch.from_numpy(y_train).float()
-        y_loc_test = torch.from_numpy(y_loc_test).float()
-        y_test = torch.from_numpy(y_test).float()
-
+        # TODO: if needed split off validation set here
         # create loaders
         train_dataset = torch.utils.data.TensorDataset(x_train, y_train.type(torch.FloatTensor))
         trainloader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
-        loc_test_dataset = torch.utils.data.TensorDataset(x_loc_test, y_loc_test.type(torch.FloatTensor))
-        loctestloader = DataLoader(loc_test_dataset, batch_size=self.batch_size, shuffle=False)
         test_dataset = torch.utils.data.TensorDataset(x_test, y_test.type(torch.FloatTensor))
         testloader = DataLoader(test_dataset, batch_size=self.batch_size, shuffle=False)
 
-        return trainloader, loctestloader, testloader
+        return trainloader, testloader
 
-class BinaryUpdate(LocalOps):
-    def __init__(self, p : DataSampler, batch_size, epochs, lr):
-        super(BinaryUpdate, self).__init__(p, batch_size, epochs, lr)
+class BinaryOps(LocalOps):
+    def __init__(self, pdata, batch_size, epochs, lr):
+        super(BinaryOps, self).__init__(pdata, batch_size, epochs, lr)
         self.criterion = nn.BCEWithLogitsLoss()
 
     def update_weights(self, model):
@@ -79,14 +58,14 @@ class BinaryUpdate(LocalOps):
                 loss.backward()
                 optimizer.step()
 
-                # if batch_idx % 10 == 0:
-                #     print('| Global Round : {} | Local Epoch : {} | [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                #         global_round+1, le+1, batch_idx * len(x),
-                #         len(self.trainloader.dataset),
-                #         100. * batch_idx / len(self.trainloader), loss.item()))
+                if batch_idx % 10 == 0:
+                    print('\r| Local Epoch : {} | [{}/{} ({:.0f}%)]\tbLoss: {:.6f}'.format(
+                        le+1, batch_idx * len(x),
+                        len(self.trainloader.dataset),
+                        100. * batch_idx / len(self.trainloader), loss.item()), end="", flush=True)
                 batch_loss.append(loss.item())
             epoch_loss.append(sum(batch_loss)/len(batch_loss))
-
+        print()
         return model.state_dict(), sum(epoch_loss) / len(epoch_loss)
 
     def inference(self, model):
@@ -95,7 +74,7 @@ class BinaryUpdate(LocalOps):
         model.eval()
         loss, total, correct = 0.0, 0.0, 0.0
 
-        for batch_idx, (x, y) in enumerate(self.loc_testloader):
+        for batch_idx, (x, y) in enumerate(self.testloader):
             x, y = x.to(self.device), y.to(self.device)
 
             # Inference
@@ -104,29 +83,29 @@ class BinaryUpdate(LocalOps):
             loss += batch_loss.item()
 
             # Prediction Binary
+            s = nn.Sigmoid()
+            pred = s(pred)
             pred[pred < 0.5] = 0
             pred[pred > 0.5] = 1
             correct += (pred == y).type(torch.float).sum().item()
             total += len(y)
 
-        accuracy = correct/total
-        return accuracy, loss
+
+        return correct, total, loss
 
 
 # TODO: use validation set to find threshold
-#  data preprocessing for values in range [0,1]
+#  data preprocessing specialties? values only in certain range?
 #  different train and testloader needed for update_weights/inference
-#  options:
-#  1
-#  -> push down definition of self.train/loc_test/glob_testloaders into subclasses, keep the common aspects in superclass
-#  -> only use normal samples in autoencoder trainloader, but both normal and malicious in loc_test/glob_testloaders
-#  2
-#  -> disregard inference in fed training loop and just use a new datasampler object for the inference that contains also malicious samples
+#  -> update_weights and inference should be done on two different instances of AutoencoderOps
+#  1. update_weights on exclusively normal samples + untrained model
+#  2. inference on new testdata with malicious samples + the trained model from 1.
 
-class AutoencoderUpdate(LocalOps):
+class AutoencoderOps(LocalOps):
     def __init__(self, p : DataSampler, batch_size, epochs, lr):
-        super(AutoencoderUpdate, self).__init__(p, batch_size, epochs, lr)
+        super(AutoencoderOps, self).__init__(p, batch_size, epochs, lr)
         self.criterion = nn.MSELoss()
+        # TODO: adapt for validation/threshold selection split
 
     def update_weights(self, model):
         # Set mode to train model
@@ -162,7 +141,6 @@ class AutoencoderUpdate(LocalOps):
         model.eval()
         loss, total, correct = 0.0, 0.0, 0.0
 
-        # TODO: testloader must contain mixed samples
         for batch_idx, (x, y) in enumerate(self.loc_testloader):
             x, y = x.to(self.device), y.to(self.device)
 
@@ -175,5 +153,4 @@ class AutoencoderUpdate(LocalOps):
             #correct += ((distance(pred,x) >= treshold) == y).type(torch.float).sum().item()
             total += len(y)
 
-        accuracy = correct/total
-        return accuracy, loss
+        return correct, total, loss

models.py
@@ -1,6 +1,6 @@
 from torch import nn
 
-
+# TODO: improve models
 class MLP(nn.Module):
     def __init__(self, K):
         super(MLP, self).__init__()

utils.py
@@ -3,7 +3,7 @@ import copy
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
+
 
 
 # TODO: further preprocessing needed? here or in localops classes
@@ -36,11 +36,18 @@ def get_sampler_data(samplers, split):
     alldata_splits = []
     for d, t in alldata:
         dlen = len(d)
+        # shuffle
+        idxs = np.arange(dlen)
+        np.random.shuffle(idxs)
+        d = d[idxs]
+        t = t[idxs]
+        # split in training and testing parts
         x_train, y_train = d[:int(split * dlen)], t[:int(split * dlen)]
         x_test, y_test = d[int(split * dlen):], t[int(split * dlen):]
         alldata_splits.append((x_train,y_train, x_test,y_test))
 
     return alldata_splits
+
 
 # returns the baseline splits for data and targets resulting from
 # the corresponding aggregated participant data and target splits
@@ -58,25 +65,28 @@ def get_baseline_data(data_per_participant):
         bx_test = np.vstack((bx_test, x_test))
         by_test = np.concatenate((by_test, y_test))
 
-    return (bx_train,by_train, bx_test,by_test)
+    return copy.deepcopy((bx_train,by_train, bx_test,by_test))
 
 
 # scales the baseline_data and returns the scaler
-def minmax_scale_baseline(alldata):
-    scaler = MinMaxScaler()
-    scaler.fit(alldata)
-    return scaler.transform(alldata), scaler
+def scale_baseline(alldata, scaler):
+    print(f"scaling baseline")
+    bx_train, by_train, bx_test, by_test = alldata
+    scaler.fit(bx_train)
+    bx_train = scaler.transform(bx_train)
+    bx_test = scaler.transform(bx_test)
 
-# requires that scaler is fitted with all data from the baseline
-def minmax_scale_federation(data_per_participant, scaler):
+    baseline_data = (bx_train,copy.deepcopy(by_train), bx_test,copy.deepcopy(by_test))
+    return baseline_data, scaler
+
+
+# requires that scaler is already fitted with all data from the baseline
+def scale_federation(data_per_participant, scaler):
+    print(f"scaling federation")
     federation_data = []
-    for d, t in data_per_participant:
-        federation_data.append((scaler.transform(d), t))
+    for x_train,y_train, x_test,y_test in data_per_participant:
+        federation_data.append((scaler.transform(x_train),copy.deepcopy(y_train), scaler.transform(x_test),copy.deepcopy(y_test)))
     return federation_data
-
-
-
-
 
 
 def average_weights(w):
@@ -91,16 +101,6 @@ def average_weights(w):
         w_avg[key] = torch.div(w_avg[key], len(w))
     return w_avg
 
-
-def get_total_sample_accuracy(corrects, totals):
-    return sum(corrects) / sum(totals)
-
-
-def get_averaged_accuracy_for_participants(corrects, totals):
-    acc = 0.0
-    for c, t in zip(corrects, totals):
-        acc += c/t
-    return acc / len(corrects)
 
 
 def plot_results(bl_losses, bl_accs, fed_losses, fed_accs, filenames):