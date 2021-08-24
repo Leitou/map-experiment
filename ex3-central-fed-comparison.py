9d56323f58f6fc8cfe17503c06a04d5140ff8f16
Leitou <timo.schenk@uzh.ch>
2021-08-24 13:48:04 +0200

    tabulate results customizable fed-cen cmp

--- ex3-central-fed-comparison.py
+++ ex3-central-fed-comparison.py
@@ -1,53 +1,35 @@
+from sys import exit
+
 import numpy as np
 import torch
-from math import floor
-from custom_types import Attack, RaspberryPi, ModelArchitecture
-from devices import Participant, Server
-from data_handler import DataHandler
-from utils import print_experiment_scores
+from tabulate import tabulate
 
-from sys import exit
+from custom_types import Attack, ModelArchitecture
+from data_handler import DataHandler
+from devices import Participant, Server
+from utils import select_federation_composition, calculate_metrics
 
 if __name__ == "__main__":
     torch.random.manual_seed(42)
     np.random.seed(42)
 
     print(f'GPU available: {torch.cuda.is_available()}')
-    print("Starting demo experiment: Federated vs Centralized Anomaly Detection")
+    print("Starting demo experiment: Federated vs Centralized Anomaly Detection, just using a single type of attack")
 
     # define collective experiment config:
-    participants_per_arch = [1,1,1,1]
-    normals = [(Attack.NORMAL, 2000)] # Adapt this value if number of participants is adapted extraordinarily
+    participants_per_arch = [1, 1, 1, 1]
+    normals = [(Attack.NORMAL, 2000)]  # Adapt this value if number of participants is adapted extraordinarily
     attacks = [val for val in Attack if val not in [Attack.NORMAL, Attack.NORMAL_V2]]
     val_percentage = 0.1
-    attack_frac = 1 / 20 # heuristic as to how much malicious samples can max be expected per participant
+    attack_frac = 1 / 5  # heuristic as to how much malicious samples can max be expected per participant
     nnorm_test = 500
-    natt_test = 100
+    natt_test_samples = 100
 
-    print(attacks)
-    # populate train and test_devices for
-    train_devices, test_devices = [], []
-    for i, num_p in enumerate(participants_per_arch):
-        for p in range(num_p):
 
-            # populate the train and val dicts
-            train_d, val_d, test_d = {}, {}, {}
-            for normal in normals:
-                train_d[normal[0]] = normal[1]
-                val_d[normal[0]] = floor(normal[1] * val_percentage)
-                if p == 0:  # add test set only once if we have this type normal
-                   test_d[normal[0]] = nnorm_test
+    train_devices, test_devices = select_federation_composition(participants_per_arch, normals, attacks, val_percentage,
+                                                                attack_frac, nnorm_test, natt_test_samples)
 
-            for attack in attacks:
-                # TODO: add here choice whether attack is in-/excluded per device? random or determ.
-                train_d[attack] = floor(normal[1] * attack_frac)
-                val_d[attack] = floor(normal[1] * attack_frac * val_percentage)
-                if p == 0:  # add test set only once if we have this type normal
-                   test_d[attack] = natt_test
-
-            train_devices.append((list(RaspberryPi)[i], train_d, val_d))
-        test_devices.append((list(RaspberryPi)[i], test_d))
-
+    print("Train Federation")
     train_sets, test_sets = DataHandler.get_all_clients_data(train_devices, test_devices)
     train_sets, test_sets = DataHandler.scale(train_sets, test_sets)
 
@@ -55,24 +37,44 @@ if __name__ == "__main__":
                     x_train, y_train, x_valid, y_valid in train_sets]
     server = Server(participants, ModelArchitecture.MLP_MONO_CLASS)
     server.train_global_model(aggregation_rounds=5)
-    # no local inference
-    x_test, y_test = test_sets[0]
-    for x, y in test_sets[1:]:
-        x_test = np.concatenate((x_test, x))
-        y_test = np.concatenate((y_test, y))
 
-    y_predicted = server.predict_using_global_model(x_test)
-    print_experiment_scores(y_test.flatten(), y_predicted.flatten().numpy(), federated=True)
+    # # no local inference
+    # x_test, y_test = test_sets[0]
+    # for x, y in test_sets[1:]:
+    #     x_test = np.concatenate((x_test, x))
+    #     y_test = np.concatenate((y_test, y))
 
-    print("------------------------------ CENTRALIZED BASELINE -----------------------")
+    # y_predicted = server.predict_using_global_model(x_test)
+    # print_experiment_scores(y_test.flatten(), y_predicted.flatten().numpy(), federated=True)
+
+    print("Train Centralized")
     x_train_all = np.concatenate(tuple(x_train for x_train, y_train, x_valid, y_valid in train_sets))
     y_train_all = np.concatenate(tuple(y_train for x_train, y_train, x_valid, y_valid in train_sets))
     x_valid_all = np.concatenate(tuple(x_valid for x_train, y_train, x_valid, y_valid in train_sets))
     y_valid_all = np.concatenate(tuple(y_valid for x_train, y_train, x_valid, y_valid in train_sets))
     central_participants = [Participant(x_train_all, y_train_all,
-                                                   x_valid_all, y_valid_all, batch_size_valid=1)]
+                                        x_valid_all, y_valid_all, batch_size_valid=1)]
     central_server = Server(central_participants, ModelArchitecture.MLP_MONO_CLASS)
     central_server.train_global_model(aggregation_rounds=5)
+    # print_experiment_scores(y_test.flatten(), y_predicted_central.flatten().numpy(), federated=False)
 
-    y_predicted_central = central_server.predict_using_global_model(x_test)
-    print_experiment_scores(y_test.flatten(), y_predicted_central.flatten().numpy(), federated=False)
+    results, central_results = [], []
+    for i, (x_test, y_test) in enumerate(test_sets):
+        y_predicted = server.predict_using_global_model(x_test)
+        y_predicted_central = central_server.predict_using_global_model(x_test)
+        attack = list(set(test_devices[i][1].keys()) - {Attack.NORMAL, Attack.NORMAL_V2})[0].value
+        normal = list(
+            set(test_devices[i][1].keys()) - {Attack.DELAY, Attack.DISORDER, Attack.FREEZE, Attack.HOP,
+                                              Attack.MIMIC,
+                                              Attack.NOISE, Attack.REPEAT, Attack.SPOOF})[0].value
+        # federated results
+        acc, f1, _ = calculate_metrics(y_test.flatten(), y_predicted.flatten().numpy())
+        results.append([test_devices[i][0], normal, attack, f'{acc * 100:.2f}%', f'{f1 * 100:.2f}%'])
+        # centralized results
+        acc, f1, _ = calculate_metrics(y_test.flatten(), y_predicted_central.flatten().numpy())
+        central_results.append([test_devices[i][0], normal, attack, f'{acc * 100:.2f}%', f'{f1 * 100:.2f}%'])
+
+    print("Federated Results")
+    print(tabulate(results, headers=['Device', 'Normal', 'Attack', 'Accuracy', 'F1 score'], tablefmt="pretty"))
+    print("Centralized Results")
+    print(tabulate(central_results, headers=['Device', 'Normal', 'Attack', 'Accuracy', 'F1 score'], tablefmt="pretty"))
--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
-from typing import Tuple, Any
+from typing import Tuple, Any, List, Dict, Union
 
 import numpy as np
+from math import floor
 from sklearn.metrics import f1_score, confusion_matrix, classification_report
+from custom_types import RaspberryPi, Attack
 
 
 def calculate_metrics(y_test: np.ndarray, y_pred: np.ndarray) -> Tuple[float, float, Any]:
@@ -23,3 +25,40 @@ def print_experiment_scores(y_test: np.ndarray, y_pred: np.ndarray, federated=Tr
           else "only single class predicted, no report generated")
     print(f"Details:\nConfusion matrix \n[(TN, FP),\n(FN, TP)]:\n{cm_fed}")
     print(f"Test Accuracy: {accuracy * 100:.2f}%, F1 score: {f1 * 100:.2f}%")
+
+
+# Assumption we test at most on what we train (attack types)
+def select_federation_composition(participants_per_arch: List, normals: List[Tuple[Attack, int]], attacks: List[Attack],
+                                  val_percentage: float, attack_frac: float, nnorm_test: int, natt_test_samples: int) \
+        -> Tuple[List[Tuple[Any, Dict[Attack, Union[int, float]], Dict[Attack, Union[int, float]]]], List[
+            Tuple[Any, Dict[Attack, int]]]]:
+    # populate train and test_devices for
+    train_devices, test_devices = [], []
+    for i, num_p in enumerate(participants_per_arch):
+        for p in range(num_p):
+
+            # add all normal monitorings for the training + validation + testing per participant
+            train_d, val_d, test_d = {}, {}, {}
+            for normal in normals:
+                train_d[normal[0]] = normal[1]
+                val_d[normal[0]] = floor(normal[1] * val_percentage)
+                test_d[normal[0]] = nnorm_test
+
+            # add all attacks for training + validation per participant
+            for attack in attacks:
+                # TODO: add here choice whether attack is in-/excluded per device? random or determ.
+                train_d[attack] = floor(normals[0][1] * attack_frac)
+                val_d[attack] = floor(normals[0][1] * attack_frac * val_percentage)
+                # if p == 0:  # add test set only once if we have this type normal
+                #     test_d[attack] = natt_test_samples
+
+            train_devices.append((list(RaspberryPi)[i], train_d, val_d))
+
+
+            # now populate the test dictionary with all selected attacks
+            for attack in attacks:
+                test_dd = dict(test_d)
+                test_dd[attack] = natt_test_samples
+                test_devices.append((list(RaspberryPi)[i], test_dd))
+
+    return train_devices, test_devices
