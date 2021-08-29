e9df0ef0f09198cee078de7b260739186c69d634
Leitou <timo.schenk@uzh.ch>
2021-08-29 20:06:57 +0200

    complete experiment scaling

--- ex3-central-fed-comparison.py
+++ ex3-central-fed-comparison.py
@@ -4,10 +4,12 @@ import numpy as np
 import torch
 from tabulate import tabulate
 
+from copy import deepcopy
 from custom_types import Behavior, ModelArchitecture
 from data_handler import DataHandler
 from devices import Participant, Server
 from utils import select_federation_composition, get_sampling_per_device, calculate_metrics
+
 
 if __name__ == "__main__":
     torch.random.manual_seed(42)
@@ -47,10 +49,11 @@ if __name__ == "__main__":
 
     print("Train Federation")
     train_sets, test_sets = DataHandler.get_all_clients_data(train_devices, test_devices)
-    train_sets, test_sets = DataHandler.scale(train_sets, test_sets)
+    train_sets_fed, test_sets_fed = deepcopy(train_sets), deepcopy(test_sets)
+    train_sets_fed, test_sets_fed = DataHandler.scale(train_sets_fed, test_sets_fed)
 
     participants = [Participant(x_train, y_train, x_valid, y_valid, batch_size_valid=1) for
-                    x_train, y_train, x_valid, y_valid in train_sets]
+                    x_train, y_train, x_valid, y_valid in train_sets_fed]
     server = Server(participants, ModelArchitecture.MLP_MONO_CLASS)
     server.train_global_model(aggregation_rounds=5)
     print()
@@ -60,26 +63,28 @@ if __name__ == "__main__":
     y_train_all = np.concatenate(tuple(y_train for x_train, y_train, x_valid, y_valid in train_sets))
     x_valid_all = np.concatenate(tuple(x_valid for x_train, y_train, x_valid, y_valid in train_sets))
     y_valid_all = np.concatenate(tuple(y_valid for x_train, y_train, x_valid, y_valid in train_sets))
-    central_participants = [Participant(x_train_all, y_train_all,
-                                        x_valid_all, y_valid_all, batch_size_valid=1)]
+    train_set_cen = [(x_train_all, y_train_all, x_valid_all, y_valid_all)]
+    train_set_cen, test_sets_cen = DataHandler.scale(train_set_cen, test_sets, central=True)
+    central_participants = [Participant(train_set_cen[0][0], train_set_cen[0][1],
+                                        train_set_cen[0][2], train_set_cen[0][3], batch_size_valid=1)]
     central_server = Server(central_participants, ModelArchitecture.MLP_MONO_CLASS)
     central_server.train_global_model(aggregation_rounds=5)
     # print_experiment_scores(y_test.flatten(), y_predicted_central.flatten().numpy(), federated=False)
 
     results, central_results = [], []
-    for i, (x_test, y_test) in enumerate(test_sets):
-        y_predicted = server.predict_using_global_model(x_test)
-        y_predicted_central = central_server.predict_using_global_model(x_test)
+    for i, (tfed, tcen) in enumerate(zip(test_sets_fed, test_sets_cen)):
+        y_predicted = server.predict_using_global_model(tfed[0])
+        y_predicted_central = central_server.predict_using_global_model(tcen[0])
         attack = list(set(test_devices[i][1].keys()) - {Behavior.NORMAL, Behavior.NORMAL_V2})[0].value
         normal = list(
             set(test_devices[i][1].keys()) - {Behavior.DELAY, Behavior.DISORDER, Behavior.FREEZE, Behavior.HOP,
                                               Behavior.MIMIC,
                                               Behavior.NOISE, Behavior.REPEAT, Behavior.SPOOF})[0].value
         # federated results
-        acc, f1, _ = calculate_metrics(y_test.flatten(), y_predicted.flatten().numpy())
+        acc, f1, _ = calculate_metrics(tfed[1].flatten(), y_predicted.flatten().numpy())
         results.append([test_devices[i][0], normal, attack, f'{acc * 100:.2f}%', f'{f1 * 100:.2f}%'])
         # centralized results
-        acc, f1, _ = calculate_metrics(y_test.flatten(), y_predicted_central.flatten().numpy())
+        acc, f1, _ = calculate_metrics(tcen[1].flatten(), y_predicted_central.flatten().numpy())
         central_results.append([test_devices[i][0], normal, attack, f'{acc * 100:.2f}%', f'{f1 * 100:.2f}%'])
 
     print("Federated Results")
