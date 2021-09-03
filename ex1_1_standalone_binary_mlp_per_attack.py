2163749ae350ee4bb5b1e45f636cc64d6560c189
Adrian Iten <adrian.iten@competec.ch>
2021-09-03 21:45:21 +0200

    Change ex1.1 setup to conform with ex1.0 setup

--- ex1_1_standalone_binary_mlp_per_attack.py
+++ ex1_1_standalone_binary_mlp_per_attack.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 import numpy as np
 import torch
 from tabulate import tabulate
@@ -16,13 +18,20 @@ if __name__ == "__main__":
 
     normals = [Behavior.NORMAL, Behavior.NORMAL_V2]
     attacks = [val for val in Behavior if val not in normals]
-    for device in RaspberryPi:
-        for normal in normals:
-            results = []
+    #attacks = [Behavior.FREEZE, Behavior.REPEAT]
+    for normal in normals:
+        other_normal = Behavior.NORMAL_V2 if normal == Behavior.NORMAL else Behavior.NORMAL
+        labels = ["Behavior"] + [pi.value for pi in RaspberryPi]
+        res_dict: Dict[RaspberryPi, Dict[Behavior, str]] = {}
+        results = []
+        for device in RaspberryPi:
+            device_dict: Dict[Behavior, str] = {}
             for attack in attacks:
                 train_sets, test_sets = DataHandler.get_all_clients_data(
                     [(device, {normal: 1500, attack: 500}, {normal: 150, attack: 50})],
-                    [(device, {normal: 500, attack: 150})])
+                    [(device, {normal: 150}),
+                     (device, {other_normal: 150}),
+                     (device, {attack: 150})])
 
                 train_sets, test_sets = DataHandler.scale(train_sets, test_sets, True)
 
@@ -31,9 +40,15 @@ if __name__ == "__main__":
                 server = Server(participants, ModelArchitecture.MLP_MONO_CLASS)
                 server.train_global_model(aggregation_rounds=5)
 
+                att_rows = []
                 for i, (x_test, y_test) in enumerate(test_sets):
                     y_predicted = server.predict_using_global_model(x_test)
-                    acc, f1, _ = calculate_metrics(y_test.flatten(), y_predicted.flatten().numpy())
-                    results.append([device.value, normal.value, attack, f'{acc * 100:.2f}%', f'{f1 * 100:.2f}%'])
+                    acc, f1, conf_mat = calculate_metrics(y_test.flatten(), y_predicted.flatten().numpy())
+                    att_rows.append([['nml', 'nml_v2', attack.value[:3]][i], f'{acc*100:.2f}%'])
+                device_dict[attack] = tabulate(att_rows, tablefmt="latex")
+            res_dict[device] = device_dict
+        for attack in attacks:
+            results.append([attack.value] + [res_dict[device][attack] for device in RaspberryPi])
 
-            print(tabulate(results, headers=['Device', 'Normal', 'Attack', 'Accuracy', 'F1 score'], tablefmt="pretty"))
+        print("Normal Behavior:", normal)
+        print(tabulate(results, headers=labels, tablefmt="latex_raw"))
