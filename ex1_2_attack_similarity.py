cce246ee7bf5d6aaee440a409f77cc2c4f2cd4f6
Adrian Iten <adrian.iten@competec.ch>
2021-09-05 21:45:17 +0200

    Add attack similarity experiment with seaborn heatmap plot

---
+++ ex1_2_attack_similarity.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import torch
+
+from custom_types import Behavior, RaspberryPi, ModelArchitecture
+from data_handler import DataHandler
+from devices import Server, Participant
+from utils import calculate_metrics
+
+if __name__ == "__main__":
+    torch.random.manual_seed(42)
+    np.random.seed(42)
+
+    print("Similarity of attacks:\n"
+          "Can the knowledge of one attack be used to detect another attack?")
+
+    normals = [Behavior.NORMAL, Behavior.NORMAL_V2]
+    attacks = [val for val in Behavior if val not in normals]
+    all_accs = []
+    device = RaspberryPi.PI4_2GB_BC
+    for attack in attacks:
+        train_sets, test_sets = DataHandler.get_all_clients_data(
+            [(device, {Behavior.NORMAL: 1000, Behavior.NORMAL_V2: 1000, attack: 500},
+              {Behavior.NORMAL: 100, Behavior.NORMAL_V2: 100, attack: 50})],
+            [(device, {other_attack: 150}) for other_attack in attacks])
+        train_sets, test_sets = DataHandler.scale(train_sets, test_sets, True)
+        participants = [Participant(x_train, y_train, x_valid, y_valid) for
+                        x_train, y_train, x_valid, y_valid in train_sets]
+        server = Server(participants, ModelArchitecture.MLP_MONO_CLASS)
+        server.train_global_model(aggregation_rounds=5)
+        att_accs = []
+        for i, (x_test, y_test) in enumerate(test_sets):
+            y_predicted = server.predict_using_global_model(x_test)
+            acc, f1, conf_mat = calculate_metrics(y_test.flatten(), y_predicted.flatten().numpy())
+            att_accs.append(acc)
+        all_accs.append(att_accs)
+    labels = [beh.value for beh in attacks]
+    hm = sns.heatmap(np.array(all_accs), xticklabels=labels, yticklabels=labels)
+    hm.get_figure().savefig("data_plot_class_similarity.png")
+    plt.show()
