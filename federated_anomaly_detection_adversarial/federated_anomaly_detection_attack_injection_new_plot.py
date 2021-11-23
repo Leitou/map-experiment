bb801590de5262fe6ba9b0cb812d1ba7c23edf8d
Adrian Iten <adrian.iten@competec.ch>
2021-11-23 20:42:25 +0100

    Subplots for Aggregation mechanisms, all device

--- federated_anomaly_detection_adversarial/federated_anomaly_detection_attack_injection_new_plot.py
+++ federated_anomaly_detection_adversarial/federated_anomaly_detection_attack_injection_new_plot.py
@@ -38,55 +38,72 @@ if __name__ == "__main__":
     for device in RaspberryPi:
         test_devices.append((device, {beh: 100 for beh in Behavior}))
 
-    for i in range(0, max_adversaries + 1):
-        train_devices = []
-        for device in RaspberryPi:
-            if device == RaspberryPi.PI4_2GB_WC:
-                continue
-            elif device == pi_to_inject:
-                train_devices += attack_devices[0:i]
-                for j in range(participants_per_device_type - i):
-                    train_devices.append((device, {Behavior.NORMAL: 1500}, {Behavior.NORMAL: 150}))
-            else:
-                for j in range(participants_per_device_type):
-                    train_devices.append((device, {Behavior.NORMAL: 1500}, {Behavior.NORMAL: 150}))
-        train_sets, test_sets = DataHandler.get_all_clients_data(
-            train_devices,
-            test_devices)
+    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(12.8, 4.8))
+    fig.suptitle(f"Injecting device {pi_to_inject.value}")
+    axs = axs.ravel().tolist()
+    # Loop that handles aggregation
+    agg_idx = 0
+    for agg in AggregationMechanism:
+        # Loop that handles 0 to n Adversaries, creating Test sets etc
+        for i in range(0, max_adversaries + 1):
+            train_devices = []
+            for device in RaspberryPi:
+                if device == RaspberryPi.PI4_2GB_WC:
+                    continue
+                elif device == pi_to_inject:
+                    train_devices += attack_devices[0:i]
+                    for j in range(participants_per_device_type - i):
+                        train_devices.append((device, {Behavior.NORMAL: 1500}, {Behavior.NORMAL: 150}))
+                else:
+                    for j in range(participants_per_device_type):
+                        train_devices.append((device, {Behavior.NORMAL: 1500}, {Behavior.NORMAL: 150}))
+            train_sets, test_sets = DataHandler.get_all_clients_data(
+                train_devices,
+                test_devices)
 
-        train_sets, test_sets = DataHandler.scale(train_sets, test_sets,
-                                                          scaling=Scaler.MINMAX_SCALER)
+            train_sets, test_sets = DataHandler.scale(train_sets, test_sets,
+                                                      scaling=Scaler.MINMAX_SCALER)
 
+            # participants contains adversaries already
+            participants = [AutoEncoderParticipant(x_train, y_train, x_valid, y_valid, batch_size_valid=1) for
+                            x_train, y_train, x_valid, y_valid in train_sets]
+            server = Server(participants, ModelArchitecture.AUTO_ENCODER,
+                            aggregation_mechanism=agg)
+            server.train_global_model(aggregation_rounds=15)
 
+            for j, (tset) in enumerate(test_sets):
+                y_predicted = server.predict_using_global_model(tset[0])
+                device = test_devices[j][0]
+                acc, f1, _ = FederationUtils.calculate_metrics(tset[1].flatten(), y_predicted.flatten().numpy())
+                test_set_result_dict['device'].append(device.value)
+                test_set_result_dict['num_adversaries'].append(i)
+                test_set_result_dict['f1'].append(f1 * 100)
 
-        # participants contains adversaries already
-        participants = [AutoEncoderParticipant(x_train, y_train, x_valid, y_valid, batch_size_valid=1) for
-                        x_train, y_train, x_valid, y_valid in train_sets]
-        server = Server(participants, ModelArchitecture.AUTO_ENCODER,
-                        aggregation_mechanism=AggregationMechanism.FED_AVG)
-        server.train_global_model(aggregation_rounds=15)
-
-        for j, (tset) in enumerate(test_sets):
-            y_predicted = server.predict_using_global_model(tset[0])
-            device = test_devices[j][0]
-            acc, f1, _ = FederationUtils.calculate_metrics(tset[1].flatten(), y_predicted.flatten().numpy())
-            test_set_result_dict['device'].append(device.value)
+            all_train, all_test = FederationUtils.aggregate_test_sets(test_sets)
+            y_predicted = server.predict_using_global_model(all_train)
+            acc, f1, _ = FederationUtils.calculate_metrics(all_test.flatten(), y_predicted.flatten().numpy())
+            test_set_result_dict['device'].append('All')
             test_set_result_dict['num_adversaries'].append(i)
             test_set_result_dict['f1'].append(f1 * 100)
 
-        all_train, all_test = FederationUtils.aggregate_test_sets(test_sets)
-        y_predicted = server.predict_using_global_model(all_train)
-        acc, f1, _ = FederationUtils.calculate_metrics(all_test.flatten(), y_predicted.flatten().numpy())
-        test_set_result_dict['device'].append('All')
-        test_set_result_dict['num_adversaries'].append(i)
-        test_set_result_dict['f1'].append(f1 * 100)
+        df = pd.DataFrame.from_dict(test_set_result_dict)
+        sns.barplot(
+            data=df, ci=None,
+            x="device", y="f1", hue="num_adversaries",
+            alpha=.6, ax=axs[agg_idx]
+        )
+        axs[agg_idx].set_ylim(0, 100)
+        axs[agg_idx].set_title(f'{agg.value}')
+        axs[agg_idx].get_legend().remove()
 
-    df = pd.DataFrame.from_dict(test_set_result_dict)
-    sns.catplot(
-        data=df, kind="bar",
-        x="device", y="f1", hue="num_adversaries",
-        alpha=.6, legend=False
-    )
-    plt.legend(bbox_to_anchor=(1, 1), loc=2, borderaxespad=0.)
-    plt.title(f'FedAvg with {pi_to_inject.value} being injected')
+        axs[agg_idx].set_ylabel('Device')
+        if agg_idx == 0:
+            axs[agg_idx].set_ylabel('F1 Score (%)')
+        else:
+            axs[agg_idx].set_ylabel(None)
+        agg_idx += 1
+    # add legend
+    handles, labels = axs[len(list(AggregationMechanism)) - 1].get_legend_handles_labels()
+    fig.legend(handles, labels, bbox_to_anchor=(1, 0.9))
+    fig.tight_layout()
     plt.show()
